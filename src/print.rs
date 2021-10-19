0a0a058de532d373368ce87f5a671651a143890c
Raja Anche <anche.raja@gmail.com>
2021-10-18 23:25:58 -0400

    Updated

--- src/print.rs
+++ src/print.rs
@@ -2,5 +2,5 @@ pub fn run() {
     println!("Hello World");
 
     println!("Hello {}", "Raja");
-    //println!("Hello {}, How is {}", "Raja", "Suma");
+    println!("Hello {}, How is {}", "Raja", "Suma");
 }
